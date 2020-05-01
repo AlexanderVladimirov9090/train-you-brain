6ec17613c59b71cd570d146e4711b952ee1ea763
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2020-05-01 13:06:39 +0300

    Loading images from Flickr API.

--- javascript/image-loader.js
+++ javascript/image-loader.js
@@ -1,27 +1,42 @@
 let slider = document.getElementById("speedRange");
 let output = document.getElementById("demo");
 let image = document.getElementById("image");
+let button = document.getElementById("trainButton");
 output.innerHTML = slider.value; // Display the default slider value
 let imagePath = "assets\\pictures\\laptop";
-let sufix='.jpg';
-let interval=1;
+let sufix = '.jpg';
+let interval = 1;
 let timeouts = [];
-  image.style.visibility='hidden';
+image.style.visibility = 'hidden';
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerHTML = this.value;
-  interval=this.value;
+  interval = this.value;
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
-} 
+}
 
-async function iterate(){
-image.style.visibility='visible';
-  for(let i=1; i <= 60; i++){
-    image.src = imagePath+i+sufix;
-    await sleep(1000/interval);
+async function iterate() {
+  if(!results){
+    return;
   }
-  image.style.visibility='hidden';
+  button.style.visibility = 'hidden';
+  preload();
+  
+  image.style.visibility = 'visible';
+  for (const eachPicture of results.photos.photo) {
+    image.src = eachPicture.picSrouce;
+    await sleep(1000 / interval);
+  }
+  image.style.visibility = 'hidden';
+  button.style.visibility = 'visible';
+  
+}
+
+function preload(){
+  for (const eachPicture of results.photos.photo) {
+    image.src = eachPicture.picSrouce;
+  }
 }
