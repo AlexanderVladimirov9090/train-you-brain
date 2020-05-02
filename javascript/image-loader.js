7b317263233476e7c5ed9389e306a4312ba9c113
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2020-05-02 09:19:22 +0300

    Adding full screen images.

--- javascript/image-loader.js
+++ javascript/image-loader.js
@@ -1,38 +1,51 @@
-let slider = document.getElementById("speedRange");
-let output = document.getElementById("demo");
-let button = document.getElementById("trainButton");
-let imageHolder = document.getElementById("imageHolder");
+let slider = document.getElementById('speedRange'); // Slider
+let output = document.getElementById('demo'); // Visual representation of slider value. 
+let button = document.getElementById('trainButton'); // Button for training
+let imageHolder = document.getElementById('imageHolder'); // Container that hold the images.
 output.innerHTML = slider.value; // Display the default slider value
-let imagePath = "assets\\pictures\\laptop";
-let sufix = '.jpg';
-let interval = slider.value;
-let timeouts = [];
-// Update the current slider value (each time you drag the slider handle)
+let sufix = '.jpg'; // image sufix.
+let interval = slider.value; // current interval.
+
+/**
+ * Sets interval from the slider.
+ *
+ */
 slider.oninput = function () {
   output.innerHTML = this.value;
   interval = this.value;
 }
 
+/**
+ * Sleep method that uses Promise.
+ * @param {milisecounds to wait} ms 
+ */
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Iterates over every image and displays it for calculated time.
+ */
 async function iterate() {
   if (!results) {
     return;
   }
   button.style.visibility = 'hidden';
   preload();
+  imageHolder.className= 'showHolder';
   for (const eachPicture of results.photos.photo) {
     let image = document.getElementById(eachPicture.id);
-    image.style.display = "block";
+    image.style.display = 'block';
     await sleep(1000 / interval);
-    image.style.display = "none";
+    image.style.display = 'none';
   }
   button.style.visibility = 'visible';
-
+  imageHolder.className = imageHolder.className.replace('showHolder', '');
 }
 
+/**
+ * Preloads images to have consistant speed over displaying images afterward.
+ */
 function preload() {
   for (const eachPicture of results.photos.photo) {
     imageHolder.appendChild(eachPicture.picSrouce)
